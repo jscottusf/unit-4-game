c287c6e8536e8022d9bd0ea87ed200390ab7fb98
Joel Scott <jscottusf@gmail.com>
2020-01-06 14:33:21 -0500

    Score displays dynamically on html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,30 @@
+var numberOptions;
+var crystalNumber;
+
 $(document).ready(function() {
+    $(".btn-lg").text("Begin");
     $(".btn-lg").on("click", function() {
+        setNumbers();
+        $(".btn-lg").text("Restart Game");
         $(document).ready(function() {
-            var targetNumber = Math.floor(Math.random() * 100) + 19;
+            var targetNumber = Math.floor(Math.random() * 120) + 19;
         $("#number-to-guess").text("Number to Guess: " + targetNumber);
             var counter = 0;
-            var numberOptions = [10, 5, 3, 7];
-            for (var i = 0; i < numberOptions.length; i++) {
+            for (var i = 0; i < 4; i++) {
+                crystalNumber = numberOptions[Math.floor(Math.random() * numberOptions.length)];
                 var imageCrystal = $("<img>");
                 imageCrystal.addClass("crystal-image");
                 imageCrystal.attr("src", "./assets/images/gem" + [i] + ".jpg");
-                imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+                imageCrystal.attr("data-crystalvalue", crystalNumber);
+                console.log(crystalNumber);
+                crystalNumbersSplice();
                 $("#crystals").append(imageCrystal);
             }
             $(".crystal-image").on("click", function() {
                 var crystalValue = ($(this).attr("data-crystalvalue"));
                 crystalValue = parseInt(crystalValue);
                 counter += crystalValue;
-                alert("New score: " + counter);
+                $("#score").text("Your Score: " + counter);
                 if (counter === targetNumber) {
                 alert("You win!");
                 $("#crystals").empty();
@@ -29,6 +37,20 @@ $(document).ready(function() {
         });
         $(".btn-lg").on("click", function() {
             $("#crystals").empty();
+            $("#score").empty();
+            setNumbers();
             });
     });
 });
+
+function crystalNumbersSplice() {
+    var index = numberOptions.indexOf(crystalNumber);
+    if (index > -1) {
+    numberOptions.splice(index, 1);
+    }
+}
+
+function setNumbers() {
+    numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    counter = 0;
+}
