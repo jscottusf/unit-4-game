128cd5c4800e7d36a04b66ce8432599fadc7bcd4
Joel Scott <jscottusf@gmail.com>
2020-01-06 13:02:50 -0500

    added images and js fuctionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             for (var i = 0; i < numberOptions.length; i++) {
                 var imageCrystal = $("<img>");
                 imageCrystal.addClass("crystal-image");
-                imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+                imageCrystal.attr("src", "./assets/images/gem" + [i] + ".jpg");
                 imageCrystal.attr("data-crystalvalue", numberOptions[i]);
                 $("#crystals").append(imageCrystal);
             }
@@ -19,9 +19,11 @@ $(document).ready(function() {
                 alert("New score: " + counter);
                 if (counter === targetNumber) {
                 alert("You win!");
+                $("#crystals").empty();
                 }
-                else if (counter >= targetNumber) {
+                else if (counter > targetNumber) {
                 alert("You lose!!");
+                $("#crystals").empty();
                 }
             });
         });
