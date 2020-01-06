8873c618d7b9787464c1acbb8d53186e110e9d9d
Joel Scott <jscottusf@gmail.com>
2020-01-06 15:57:28 -0500

    sounds added...so much easier with JQ

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,22 @@ var numberOptions;
 var crystalNumber;
 
 $(document).ready(function() {
+    var clickElement = document.createElement("audio");
+    clickElement.setAttribute("src", "./assets/sounds/chime.wav");
+    var winElement = document.createElement("audio");
+    winElement.setAttribute("src", "./assets/sounds/win.wav");
+    var lossElement = document.createElement("audio");
+    lossElement.setAttribute("src", "./assets/sounds/loss.wav");
+    var drumElement = document.createElement("audio");
+    drumElement.setAttribute("src", "./assets/sounds/drums.wav");
+
     $(".btn-lg").text("Begin");
     $(".btn-lg").on("click", function() {
+        drumElement.play();
         setNumbers();
         $(".btn-lg").text("Restart Game");
         $(document).ready(function() {
-            var targetNumber = Math.floor(Math.random() * 120) + 19;
+            var targetNumber = Math.floor(Math.random() * 101) + 19;
         $("#number-to-guess").text("Number to Guess: " + targetNumber);
             var counter = 0;
             for (var i = 0; i < 4; i++) {
@@ -21,23 +31,26 @@ $(document).ready(function() {
                 $("#crystals").append(imageCrystal);
             }
             $(".crystal-image").on("click", function() {
+                clickElement.play();
                 var crystalValue = ($(this).attr("data-crystalvalue"));
                 crystalValue = parseInt(crystalValue);
                 counter += crystalValue;
                 $("#score").text("Your Score: " + counter);
                 if (counter === targetNumber) {
-                alert("You win!");
+                clickElement.pause();
+                winElement.play();
+                $("#winner").text("Winner winner chicken dinner! Try again?");
                 $("#crystals").empty();
                 }
                 else if (counter > targetNumber) {
-                alert("You lose!!");
+                clickElement.pause();
+                lossElement.play();
+                $("#winner").text("Try again, loser");
                 $("#crystals").empty();
                 }
             });
         });
         $(".btn-lg").on("click", function() {
-            $("#crystals").empty();
-            $("#score").empty();
             setNumbers();
             });
     });
@@ -53,4 +66,7 @@ function crystalNumbersSplice() {
 function setNumbers() {
     numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     counter = 0;
+    $("#crystals").empty();
+    $("#score").empty();
+    $("#winner").empty();
 }
